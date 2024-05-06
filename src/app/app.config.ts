16d52f2b3f7bba8ec53c29591f214dfee6351292
Justin Torres <justinralphtorres@gmail.com>
2024-05-06 13:32:06 +0800

    remove unnecessary import

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideClientHydration } from '@angular/platform-browser';
 import { routes } from './app.routes';
 import {
   provideHttpClient,
