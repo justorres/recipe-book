26269e2556882960664db5bc6f64a78e352e7c13
Justin Torres <justinralphtorres@gmail.com>
2024-05-06 11:27:09 +0800

    removed provideClientHydration()

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,7 +11,6 @@ import { authInterceptor } from './shared/interceptors/auth.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideClientHydration(),
     provideHttpClient(withInterceptors([authInterceptor]))
   ]
 };
